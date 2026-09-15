162099a939f1535f8b9d6c1ce27c0af7d4a15930
ayoub houdda <houddaayoub34@gmail.com>
2026-09-15 15:21:21 +0100

    L'ajout de la fonction createUser

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -10,6 +10,20 @@ const findUserByEmail = async (email) => {
     return result.rows[0];
 };
 
+const createUser = async(firstName, lastName, email, passwored){
+    const result = await pool.query(
+        `
+        INSERT INTO users (first_name, last_name, email, password)
+        VALUES ($1, $2, $3, $4)
+        RETURNING id, firs_name, last_name, email, role, email_verified
+        `,
+        [firstName, lastName, email, passwored]
+         );
+         return result.rows[0];
+
+}
+
 module.exports = {
-    findUserByEmail
+    findUserByEmail,
+    createUser
 };
