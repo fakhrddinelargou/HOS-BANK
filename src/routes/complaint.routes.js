8cd20bca945c05b008047b919c019563c414d95d
ayoub houdda <houddaayoub34@gmail.com>
2026-09-22 10:05:46 +0000

    Ajouter le router pour prendre toute les réclamations et pour prendre une seule réclamation en les testant avec postman

--- src/routes/complaint.routes.js
+++ src/routes/complaint.routes.js
@@ -2,8 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
-const {create} = require("../controllers/complaint.controller");
+const {create, getAll, getOne} = require("../controllers/complaint.controller");
+const { isAuthenticated } = require("../middlewares/auth.middleware");
 
-router.post("/create", create);
+router.post("/create", isAuthenticated, create);
+router.get("/",isAuthenticated, getAll);
+router.get("/:id",isAuthenticated, getOne);
 
 module.exports = router;
