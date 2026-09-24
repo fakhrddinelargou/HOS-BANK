fa1ce608702c06be68bfe1fe2a661a8ebaa7aa3d
fakhreddinelargou <largoufakhreddine@gmail.com>
2026-09-24 12:15:19 +0100

    HOS-83 fix middleware role

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -9,19 +9,6 @@ const isAuthenticated = (req, res, next)=>{
 }
 
 
-const authorizeRole = (...allowedRoles)=>{
-
-    return (req, res, next)=>{
-
-        const userRole = req.session.role;
-        if(!allowedRoles.includes(userRole)){
-            return res.status(403).json({
-                message: "Accès interdit"
-            })
-        }
-        next();
-    }
-}
 
 
 module.exports = {
