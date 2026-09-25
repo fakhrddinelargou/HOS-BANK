ee89b656d04afcbbff0fa2907145cbb44ee55ff9
ayoub houdda <houddaayoub34@gmail.com>
2026-09-25 17:10:50 +0000

    Installer et configuer ejs pour le front-end

--- src/server.js
+++ src/server.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const app = express();
+//routes
 const authRoutes = require("./routes/auth.routes");
 const ribRoutes = require("./routes/rib.routes");
 const complaintRoutes = require("./routes/complaint.routes");
 const compteRouter = require('./routes/compte.routes');
-const beneficiariesRouter = require('./routes/beneficiaires.routes')
+const beneficiariesRouter = require('./routes/beneficiaires.routes');
+const viewRoutes = require("./routes/view.routes");
+//session
 const session = require("express-session");
-require("dotenv").config();
-
-
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false
 }));
+
+const path = require("path");
+require("dotenv").config();
+//views
+app.set('view engine', "ejs");
+app.set("views", path.join(__dirnamedirname, "views"));
+
 
 // 3. Define the port environment variable (defaulting to 3000)
 const PORT = process.env.PORT || 3000;
@@ -29,7 +36,8 @@ app.use('/rib', ribRoutes);
 //auth
 app.use('/auth', authRoutes);
 app.use('/beneficiaries', beneficiariesRouter);
-
+//views
+app.use('/', viewRoutes);
 
 //complaint
 app.use('/complaint', complaintRoutes);
